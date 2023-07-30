95daa144b0de9f4348af3a51da7dab5780693583
tanle0853 <tanlt2@rikkeisoft.com>
2023-07-31 00:48:14 +0900

    add login

---
+++ my-login-app/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Login />
+  </div>
+</template>
+
+<script>
+import Login from "./components/Login.vue";
+
+export default {
+  components: {
+    Login,
+  },
+};
+</script>
+
+<style>
+/* Thêm CSS cho component cha (tuỳ chọn) */
+</style>
---
+++ my-login-app/src/components/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h2>Login</h2>
+    <form @submit.prevent="login">
+      <div>
+        <label for="username">Username:</label>
+        <input type="text" v-model="username" id="username" required />
+      </div>
+      <div>
+        <label for="password">Password:</label>
+        <input type="password" v-model="password" id="password" required />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      // Thực hiện xử lý đăng nhập tại đây, ví dụ:
+      console.log("Username:", this.username);
+      console.log("Password:", this.password);
+    },
+  },
+};
+</script>
+
+<style>
+/* Thêm CSS cho component đăng nhập (tuỳ chọn) */
+</style>
